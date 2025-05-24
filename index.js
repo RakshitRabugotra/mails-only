8518fdc125724f70319e7366b72e00cbb4d18596
RakshitRabugotra <rakshit.rabugotra000@gmail.com>
2025-05-24 19:25:32 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -2,8 +2,25 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native"
+import App from "./App"
+import { name as appName } from "./app.json"
 
-AppRegistry.registerComponent(appName, () => App);
+import { PaperProvider } from "react-native-paper"
+import { NavigationContainer } from "@react-navigation/native"
+
+import useTheme from "./src/hooks/use-theme"
+
+export default function Main() {
+  const theme = useTheme()
+
+  return (
+    <PaperProvider theme={theme}>
+      <NavigationContainer theme={theme}>
+        <App />
+      </NavigationContainer>
+    </PaperProvider>
+  )
+}
+
+AppRegistry.registerComponent(appName, () => Main)
