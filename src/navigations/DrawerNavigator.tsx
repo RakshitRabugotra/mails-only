1db7c165ba3c8a0ede5475fb9f6582417bf6844d
RakshitRabugotra <rakshit.rabugotra000@gmail.com>
2025-05-26 16:23:51 +0530

    fix: prevent back press on drawer home screen

--- src/navigations/DrawerNavigator.tsx
+++ src/navigations/DrawerNavigator.tsx
@@ -1,6 +1,7 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import TabNavigator from './TabNavigator';
 import MockScreen from '../screens/MockScreens';
+import PreventBackPress from "../components/prevent-back-press"
 
 export type DrawerParamList = {
   Home: undefined
@@ -9,19 +10,21 @@ export type DrawerParamList = {
   Drafts: undefined
 }
 
-const Drawer = createDrawerNavigator();
+const Drawer = createDrawerNavigator()
 
 export default function DrawerNavigator() {
   return (
-    <Drawer.Navigator
-      screenOptions={{
-        headerShown: false
-      }}
-    >
-      <Drawer.Screen name="Home" component={TabNavigator} />
-      <Drawer.Screen name="Snoozed" component={MockScreen} />
-      <Drawer.Screen name="Sent" component={MockScreen} />
-      <Drawer.Screen name="Drafts" component={MockScreen} />
-    </Drawer.Navigator>
-  );
+    <PreventBackPress allowOnConfirm>
+      <Drawer.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Drawer.Screen name="Home" component={TabNavigator} />
+        <Drawer.Screen name="Snoozed" component={MockScreen} />
+        <Drawer.Screen name="Sent" component={MockScreen} />
+        <Drawer.Screen name="Drafts" component={MockScreen} />
+      </Drawer.Navigator>
+    </PreventBackPress>
+  )
 }
